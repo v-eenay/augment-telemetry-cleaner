524bd932cfed97b1a1e952c2f40d7e4a0cc2b3be
v-eenay <koiralavinay@gmail.com>
2025-08-01 10:22:21 +0545

    Add app icon from assets/icon.png

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"augment-telemetry-cleaner/internal/gui"
+	"augment-telemetry-cleaner/internal/resources"
 )
 
 func main() {
 	myApp := app.NewWithID("com.vinaykoirala.augmenttelemetrycleaner")
+	myApp.SetIcon(resources.ResourceIconPng)
 
 	mainWindow := myApp.NewWindow("Augment Telemetry Cleaner v2.0.0")
+	mainWindow.SetIcon(resources.ResourceIconPng)
 	mainWindow.Resize(fyne.NewSize(800, 700))
 	mainWindow.CenterOnScreen()
 
